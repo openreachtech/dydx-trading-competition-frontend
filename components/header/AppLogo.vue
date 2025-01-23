b2b8c2421e9aa517d2a69dd4b8442b862bebf5d5
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-01-23 08:40:01 +0700

    Build `AppLogo` component for header

---
+++ components/header/AppLogo.vue
@@ -0,0 +1,49 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import {
+  NuxtLink,
+  Icon,
+} from '#components'
+
+export default defineComponent({
+  components: {
+    Icon,
+    NuxtLink,
+  },
+})
+</script>
+
+<template>
+  <NuxtLink to="/"
+    class="unit-logo"
+  >
+    <Icon name="heroicons-solid:home"
+      size="1.5rem"
+      class="icon"
+    />
+
+    <img src="/img/logo-medium.png">
+  </NuxtLink>
+</template>
+
+<style scoped>
+.unit-logo {
+  display: inline-flex;
+  align-items: center;
+  gap: 1.5rem;
+
+  /* NOTE: Can remove if reset color in reset stylesheet. */
+  color: inherit;
+}
+
+.unit-logo > .icon {
+  display: none;
+
+  @media (30rem <= width) {
+    display: inline;
+  }
+}
+</style>
