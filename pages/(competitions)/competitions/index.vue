b7a12556c9981e5024e255f9d9c08b0cb531a543
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-04 18:11:32 +0700

    Set `/competitions` as index route

--- pages/(competitions)/competitions/index.vue
+++ pages/(competitions)/competitions/index.vue
@@ -1,3 +1,21 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import {
+  definePageMeta,
+} from '#imports'
+
+export default defineComponent({
+  setup () {
+    definePageMeta({
+      alias: '/',
+    })
+  },
+})
+</script>
+
 <template>
   <!-- TODO: Fulfill page -->
   <div>
