414637e4dd4545d7c5534cdcaf5a3718c59c159c
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-05 22:15:16 +0700

    Create context for AppLeagueCard component

---
+++ app/vue/contexts/AppLeagueCardContext.js
@@ -0,0 +1,171 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+import {
+  COMPETITION_STATUS,
+} from '~/app/constants'
+
+/**
+ * @import { CompetitionEntity } from '~/app/graphql/client/queries/competitions/CompetitionsQueryGraphqlCapsule'
+ */
+
+/**
+ * Context class for AppLeagueCard component.
+ *
+ * @extends {BaseFuroContext<null>} - Base class.
+ */
+export default class AppLeagueCardContext extends BaseFuroContext {
+  /**
+   * Extracts a specific competition value.
+   *
+   * @template {keyof CompetitionEntity} T
+   * @param {T} key - A property of competition.
+   * @returns {CompetitionEntity[T] | null} Coresponding property's value, or `null` if not found.
+   */
+  extractCompetitionByKey (key) {
+    return this.props
+      .competition[key]
+      ?? null
+  }
+
+  /**
+   * Generate badge severity.
+   *
+   * @returns {string} Badge severity.
+   */
+  generateBadgeSeverity () {
+    const fallbackValue = 'neutral'
+    const { statusId } = this.extractCompetitionByKey('status') ?? {}
+
+    if (!statusId) {
+      return fallbackValue
+    }
+
+    const severityHashMap = {
+      [COMPETITION_STATUS.CREATED.ID]: 'success',
+      [COMPETITION_STATUS.REGISTRATION_ENDED.ID]: 'warn',
+      [COMPETITION_STATUS.IN_PROGRESS.ID]: 'info',
+      [COMPETITION_STATUS.COMPLETED.ID]: 'completed',
+      [COMPETITION_STATUS.CANCELED.ID]: 'canceled',
+    }
+
+    return severityHashMap[statusId] ?? fallbackValue
+  }
+
+  /**
+   * Generate badge description.
+   *
+   * @returns {string} Badge description.
+   */
+  generateBadgeDescription () {
+    const { statusId } = this.extractCompetitionByKey('status') ?? {}
+
+    return Object.values(COMPETITION_STATUS)
+      .find(it => it.ID === statusId)
+      ?.DESCRIPTION
+      ?? '--'
+  }
+
+  /**
+   * Generate icon name for badge.
+   *
+   * @returns {string} Icon name.
+   */
+  generateBadgeIconName () {
+    const fallbackValue = ''
+    const { statusId } = this.extractCompetitionByKey('status') ?? {}
+
+    if (!statusId) {
+      return fallbackValue
+    }
+
+    const iconNameHashMap = {
+      [COMPETITION_STATUS.COMPLETED.ID]: 'heroicons:check-16-solid',
+      [COMPETITION_STATUS.CANCELED.ID]: 'heroicons:x-mark-16-solid',
+    }
+
+    return iconNameHashMap[statusId] ?? fallbackValue
+  }
+
+  /**
+   * Generate image url
+   *
+   * @returns {string} Image URL
+   */
+  genenrateImageUrl () {
+    return this.extractCompetitionByKey('imageUrl') ?? ''
+  }
+
+  /**
+   * Generate destination.
+   *
+   * @returns {string} Route string.
+   */
+  generateDestination () {
+    const competitionId = this.extractCompetitionByKey('competitionId')
+
+    return `/competitions/${competitionId}`
+  }
+
+  /**
+   * Normalize minimum deposit.
+   *
+   * @returns {string} Normalized minimum deposit.
+   */
+  normalizeMinimumDeposit () {
+    const normalizedFigure = this.normalizeNumber({
+      value: this.extractCompetitionByKey('minimumDeposit'),
+    })
+
+    return `${normalizedFigure} USDC`
+  }
+
+  /**
+   * Normalize number.
+   *
+   * @param {{
+   *   value: number | null
+   *   options?: Intl.NumberFormatOptions
+   * }} params - Parameters.
+   * @returns {string} Normalized number string.
+   */
+  normalizeNumber ({
+    value,
+    options = {},
+  }) {
+    if (!value) {
+      return '--'
+    }
+
+    const formatter = new Intl.NumberFormat('en-US', {
+      trailingZeroDisplay: 'stripIfInteger',
+      ...options,
+    })
+
+    return formatter.format(value)
+  }
+
+  /**
+   * Normalize currency.
+   *
+   * @param {{
+   *   value: number | null
+   *   options?: Intl.NumberFormatOptions
+   * }} params - Parameters.
+   * @returns {string} Normalized currency string.
+   */
+  normalizeCurrency ({
+    value,
+    options = {},
+  }) {
+    return this.normalizeNumber({
+      value,
+      options: {
+        style: 'currency',
+        currency: 'USD',
+        ...options,
+      },
+    })
+  }
+}
