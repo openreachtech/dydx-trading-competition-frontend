4e7d157bd98df7f950ff8cca83d43d92461fe7d8
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-06 09:08:06 +0700

    Create GraphQL definitions for competitions query

---
+++ app/graphql/client/queries/competitions/CompetitionsQueryGraphqlCapsule.js
@@ -0,0 +1,60 @@
+import BaseAppGraphqlCapsule from '~/app/graphql/client/BaseAppGraphqlCapsule'
+
+/**
+ * Competitions query graphql capsule.
+ *
+ * @extends {BaseAppGraphqlCapsule<typeof CompetitionsQueryGraphqlCapsule, CompetitionsQueryResponseContent>}
+ */
+export default class CompetitionsQueryGraphqlCapsule extends BaseAppGraphqlCapsule {
+  /**
+   * Extract competitions.
+   *
+   * @returns {CompetitionsQueryResponseContent['competitions']['competitions']} An array of competition.
+   */
+  extractCompetitions () {
+    const content = this.extractContent()
+
+    return content
+      ?.competitions
+      ?.competitions
+      ?? []
+  }
+}
+
+/**
+ * @typedef {{
+ *   competitions: {
+ *     competitions: Array<CompetitionEntity>
+ *   }
+ * }} CompetitionsQueryResponseContent
+ */
+
+/**
+ * @typedef {{
+ *   competitionId: number
+ *   title: string
+ *   description: string
+ *   participantUpperLimit: number
+ *   participantLowerLimit: number
+ *   host: {
+ *     address: string
+ *     name: string
+ *   }
+ *   totalPrize: number
+ *   minimumDeposit: number
+ *   imageUrl?: string
+ *   schedules: Array<{
+ *     categogry: {
+ *       categoryId: number
+ *       name: string
+ *       description: string
+ *     }
+ *     scheduledDatetime: string
+ *   }>
+ *   status: {
+ *     statusId: number
+ *     name: string
+ *     phasedAt: string
+ *   }
+ * }} CompetitionEntity
+ */
---
+++ app/graphql/client/queries/competitions/CompetitionsQueryGraphqlLauncher.js
@@ -0,0 +1,20 @@
+import BaseAppGraphqlLauncher from '~/app/graphql/client/BaseAppGraphqlLauncher'
+import CompetitionsQueryGraphqlPayload from '~/app/graphql/client/queries/competitions/CompetitionsQueryGraphqlPayload'
+import CompetitionsQueryGraphqlCapsule from '~/app/graphql/client/queries/competitions/CompetitionsQueryGraphqlCapsule'
+
+/**
+ * Competitions query graphql launcher.
+ *
+ * @extends {BaseAppGraphqlLauncher<typeof CompetitionsQueryGraphqlLauncher>}
+ */
+export default class CompetitionsQueryGraphqlLauncher extends BaseAppGraphqlLauncher {
+  /** @override */
+  static get Payload () {
+    return CompetitionsQueryGraphqlPayload
+  }
+
+  /** @override */
+  static get Capsule () {
+    return CompetitionsQueryGraphqlCapsule
+  }
+}
