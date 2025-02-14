94a25e59f2fd39c46e2fcea779b6d3eccfc96d96
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-14 14:06:48 +0700

    Build context for AppInput

---
+++ app/vue/contexts/AppInputContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * Props context class for AppInput component.
+ *
+ * @extends {BaseFuroContext<null>} - Base class.
+ */
+export default class AppInputContext extends BaseFuroContext {
+
+}
