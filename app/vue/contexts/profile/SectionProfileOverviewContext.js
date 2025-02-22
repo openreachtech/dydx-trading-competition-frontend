72b04ae43aa8e873e7f17813c7d0fd350a6db3d0
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-23 00:01:20 +0700

    Build context for SectionProfileOverview

---
+++ app/vue/contexts/profile/SectionProfileOverviewContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * SectionProfileOverviewContext
+ *
+ * @extends {BaseFuroContext<null>}
+ */
+export default class SectionProfileOverviewContext extends BaseFuroContext {
+
+}
