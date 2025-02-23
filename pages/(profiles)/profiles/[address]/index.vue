1db8e9abb9316abda7681cae3798c45fbb6e023b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-23 12:08:45 +0700

    Add SectionProfileOverview to `profiles/[address]`

--- pages/(profiles)/profiles/[address]/index.vue
+++ pages/(profiles)/profiles/[address]/index.vue
@@ -1,6 +1,29 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import SectionProfileOverview from '~/components/profile/SectionProfileOverview.vue'
+
+export default defineComponent({
+  components: {
+    SectionProfileOverview,
+  },
+})
+</script>
+
 <template>
-  <!-- TODO: Fulfill page -->
-  <div>
-    Profile Address
+  <div class="unit-page">
+    <SectionProfileOverview />
   </div>
 </template>
+
+<style scoped>
+.unit-page {
+  margin-inline: calc(-1 * var(--size-body-padding-inline-mobile));
+
+  @media (30rem < width) {
+    margin-inline: calc(-1 * var(--size-body-padding-inline-desktop));
+  }
+}
+</style>
