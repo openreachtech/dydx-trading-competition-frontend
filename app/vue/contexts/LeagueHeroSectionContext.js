ff8c389fa02a131e0b460b8c224893d4d6535790
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-02-27 13:35:23 +0700

    Create context class for LeagueHeroSection

---
+++ app/vue/contexts/LeagueHeroSectionContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * LeagueHeroSectionContext
+ *
+ * @extends {BaseFuroContext<null>}
+ */
+export default class LeagueHeroSectionContext extends BaseFuroContext {
+
+}
