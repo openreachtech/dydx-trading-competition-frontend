7561c7269989881612bbcf75dd8cd5e25744a6f6
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-06 11:19:46 +0700

    Add header slot for AppDialog

--- app/vue/contexts/AppDialogContext.js
+++ app/vue/contexts/AppDialogContext.js
@@ -8,5 +8,23 @@ import {
  * @extends {FuroButtonDialogContext}
  */
 export default class AppDialogContext extends FuroButtonDialogContext {
+  /**
+   * get: title
+   *
+   * @returns {string} Dialog's title.
+   */
+  get title () {
+    return this.props.title
+  }
 
+  /**
+   * Generate title classes.
+   *
+   * @returns {Record<string, boolean>} Title classes.
+   */
+  generateTitleClasses () {
+    return {
+      hidden: this.title === null,
+    }
+  }
 }
--- components/units/AppDialog.vue
+++ components/units/AppDialog.vue
@@ -8,6 +8,10 @@ import {
   ref,
 } from 'vue'
 
+import {
+  Icon,
+} from '#components'
+
 import FuroDialog from '@openreachtech/furo-nuxt/lib/components/FuroDialog.vue'
 
 import AppDialogContext from '~/app/vue/contexts/AppDialogContext'
@@ -16,10 +20,22 @@ export default defineComponent({
   name: 'AppDialog',
 
   components: {
+    Icon,
     FuroDialog,
   },
 
   inheritAttrs: false,
+
+  props: {
+    title: {
+      type: [
+        String,
+        null,
+      ],
+      required: false,
+      default: null,
+    },
+  },
 
   setup (
     props,
@@ -40,6 +56,7 @@ export default defineComponent({
 
     return {
       dialogComponentRef,
+      context,
     }
   },
 })
@@ -51,6 +68,24 @@ export default defineComponent({
     v-bind="$attrs"
   >
     <template #contents>
+      <slot name="header">
+        <div class="unit-header">
+          <span class="title"
+            :class="context.generateTitleClasses()"
+          >
+            {{ context.title }}
+          </span>
+
+          <button class="button close"
+            @click="context.dismissDialog()"
+          >
+            <Icon name="heroicons:x-mark"
+              size="1.5rem"
+            />
+          </button>
+        </div>
+      </slot>
+
       <slot name="contents" />
     </template>
   </FuroDialog>
@@ -71,5 +106,39 @@ export default defineComponent({
 
     background-color: var(--color-background-dialog);
   }
+}
+</style>
+
+<style scoped>
+.unit-header {
+  padding-block-end: 0.75rem;
+
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 0.5rem;
+}
+
+.unit-header > .title {
+  font-size: var(--font-size-medium);
+  font-weight: 500;
+  line-height: var(--size-line-height-medium);
+
+  color: var(--color-text-secondary);
+}
+
+.unit-header > .title.hidden {
+  display: none;
+}
+
+.unit-header > .button.close {
+  color: var(--color-text-tertiary);
+
+  justify-self: end;
+
+  transition: color 250ms var(--transition-timing-base);
+}
+
+.unit-header > .button.close:hover {
+  color: var(--color-text-primary);
 }
 </style>
