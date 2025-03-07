0747370120dd0682fd65a9ed631667497c0bd22b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-07 15:37:09 +0700

    Build `AppOnboardingControl` component

---
+++ components/units/AppOnboardingControl.vue
@@ -0,0 +1,52 @@
+<script>
+import {
+  defineComponent,
+  ref,
+} from 'vue'
+
+import AppButton from '~/components/units/AppButton.vue'
+import WalletSelectionDialog from '~/components/dialogs/WalletSelectionDialog.vue'
+
+import AppOnboardingControlContext from '~/app/vue/contexts/AppOnboardingControlContext'
+
+export default defineComponent({
+  components: {
+    AppButton,
+    WalletSelectionDialog,
+  },
+
+  setup (
+    props,
+    componentContext
+  ) {
+    /** @type {import('vue').Ref<import('~/components/units/AppDialog.vue').default | null>} */
+    const walletSelectionDialogRef = ref(null)
+
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = AppOnboardingControlContext.create(args)
+      .setupComponent()
+
+    return {
+      walletSelectionDialogRef,
+
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <div>
+    <AppButton @click="context.showWalletSelectionDialog({
+      dialogElement: walletSelectionDialogRef,
+    })"
+    >
+      Connect Wallet
+    </AppButton>
+
+    <WalletSelectionDialog ref="walletSelectionDialogRef" />
+  </div>
+</template>
