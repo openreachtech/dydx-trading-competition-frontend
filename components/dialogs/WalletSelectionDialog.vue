c7386f5ba23937ccc061430af887d956e6b2dc02
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-07 14:52:05 +0700

    Build `WalletSelectionDialog` component

---
+++ components/dialogs/WalletSelectionDialog.vue
@@ -0,0 +1,173 @@
+<script>
+import {
+  defineComponent,
+  ref,
+} from 'vue'
+
+import {
+  Icon,
+  NuxtLink,
+} from '#components'
+
+import AppButton from '~/components/units/AppButton.vue'
+import AppDialog from '~/components/units/AppDialog.vue'
+
+import WalletSelectionDialogContext from '~/app/vue/contexts/dialogs/WalletSelectionDialogContext'
+
+export default defineComponent({
+  components: {
+    Icon,
+    NuxtLink,
+    AppButton,
+    AppDialog,
+  },
+
+  setup (
+    props,
+    componentContext
+  ) {
+    // NOTE: Actualy type is `AppDialog` but type declaration is `FuroDialog`.
+    /** @type {import('vue').Ref<import('@openreachtech/furo-nuxt/lib/components/FuroDialog.vue').default | null>} */
+    const dialogComponentRef = ref(null)
+
+    const args = {
+      props,
+      componentContext,
+      dialogComponentRef,
+    }
+    const context = WalletSelectionDialogContext.create(args)
+      .setupComponent()
+
+    return {
+      dialogComponentRef,
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <AppDialog ref="dialogComponentRef"
+    class="unit-dialog"
+  >
+    <template #title>
+      <span class="unit-title">
+        Connect Your Wallet
+      </span>
+    </template>
+
+    <template #contents>
+      <div class="unit-contents">
+        <p class="description">
+          By connecting a wallet, you agree to DYDX Foundation
+          <NuxtLink to="#"
+            class="link"
+            external
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Terms of Service
+          </NuxtLink>
+          and consent to its
+          <NuxtLink to="#"
+            class="link"
+            external
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Privacy Policy
+          </NuxtLink>
+        </p>
+
+        <div class="actions">
+          <AppButton v-for="it of context.supportedWallets"
+            variant="neutral"
+          >
+            <template #startIcon>
+              <img src="/img/wallets/metamask.svg"
+                :alt="it.label"
+              >
+            </template>
+
+            <template #default>
+              {{ it.label }}
+            </template>
+          </AppButton>
+        </div>
+
+        <NuxtLink class="link"
+          to="#"
+        >
+          <span>
+            Learn about wallet
+          </span>
+
+          <Icon name="heroicons-outline:external-link"
+            size="1rem"
+          />
+        </NuxtLink>
+      </div>
+    </template>
+  </AppDialog>
+</template>
+
+<style scoped>
+.unit-dialog {
+  max-width: 30rem;
+}
+
+.unit-title {
+  font-size: var(--font-size-large);
+  font-weight: 500;
+  line-height: var(--size-line-height-large);
+
+  color: var(--color-text-primary);
+}
+
+.unit-contents {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.unit-contents > .description {
+  font-size: var(--font-size-small);
+
+  color: var(--color-text-placeholder);
+}
+
+.unit-contents > .description > .link {
+  font-weight: 700;
+
+  color: inherit;
+}
+
+.unit-contents > .description > .link:hover {
+  text-decoration: underline;
+}
+
+.unit-contents > .actions {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.75rem;
+}
+
+.unit-contents > .link {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  align-self: center;
+
+  font-size: var(--font-size-base);
+  font-weight: 500;
+
+  color: var(--color-text-tertiary);
+
+  transition: color 250ms var(--transition-timing-base);
+}
+
+.unit-contents > .link:hover {
+  color: var(--color-text-primary);
+}
+</style>
