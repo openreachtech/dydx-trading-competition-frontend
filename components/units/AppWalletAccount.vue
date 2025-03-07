a1d1ff14aa3e1b48ffc23bc89e1826508717ea30
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-07 17:17:59 +0700

    Build `AppWalletAccount` component

---
+++ components/units/AppWalletAccount.vue
@@ -0,0 +1,164 @@
+<script>
+import {
+  defineComponent,
+  ref,
+} from 'vue'
+
+import {
+  Icon,
+  NuxtLink,
+} from '#components'
+
+import WalletAccountContext from '~/app/vue/contexts/AppWalletAccountContext'
+
+export default defineComponent({
+  components: {
+    Icon,
+    NuxtLink,
+  },
+
+  setup (
+    props,
+    componentContext
+  ) {
+    const isShowingDropdownRef = ref(false)
+
+    const args = {
+      props,
+      componentContext,
+      isShowingDropdownRef,
+    }
+    const context = WalletAccountContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="unit-account"
+    :class="context.generateDropdownClasses()"
+  >
+    <button class="button"
+      @click="context.toggleDropdown"
+    >
+      <img src="/img/wallets/metamask.svg"
+        alt="Metamask"
+      >
+
+      <span>0xDcf2ED...d10c</span>
+
+      <Icon name="heroicons:chevron-down-16-solid"
+        size="1rem"
+      />
+    </button>
+
+    <div class="unit-dropdown">
+      <div class="actions">
+        <NuxtLink class="button">
+          <Icon name="heroicons:user"
+            size="1rem"
+          />
+
+          <span>My league profile</span>
+        </NuxtLink>
+
+        <NuxtLink to="https://dydx.trade"
+          external
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button"
+        >
+          <Icon name="heroicons:home-solid"
+            size="1rem"
+          />
+
+          <span>Open dydx.trade</span>
+        </NuxtLink>
+
+        <button class="button">
+          <Icon name="heroicons:arrow-right-start-on-rectangle"
+            size="1rem"
+          />
+
+          <span>Disconnect wallet</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.unit-account {
+  position: relative;
+}
+
+.unit-account > .button {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  font-size: var(--font-size-base);
+  font-weight: 500;
+
+  color: var(--color-text-secondary);
+}
+
+.unit-account.show-dropdown > .unit-dropdown {
+  display: block;
+}
+
+.unit-dropdown {
+  border-radius: 1rem;
+  border-width: var(--size-thinnest);
+  border-style: solid;
+  border-color: var(--color-border-dropdown);
+
+  padding-block: 1.25rem;
+  padding-inline: 1.25rem;
+
+  display: none;
+
+  position: absolute;
+  top: calc(100% + 0.75rem);
+  right: 0;
+
+  width: 17.5rem;
+
+  background-color: var(--color-background-dropdown);
+}
+
+.unit-dropdown > .actions {
+  margin-block-start: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+.unit-dropdown > .actions > .button {
+  border-radius: 0.375rem;
+
+  padding-block: 0.4375rem;
+  padding-inline: 0.5rem;
+
+  display: inline-flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  font-size: var(--font-size-small);
+  line-height: var(--size-line-height-small);
+
+  background-color: var(--color-background-dropdown);
+  color: var(--color-text-secondary);
+
+  transition: filter 250ms var(--transition-timing-base);
+}
+
+.unit-dropdown > .actions > .button:hover {
+  filter: brightness(1.4);
+}
+</style>
