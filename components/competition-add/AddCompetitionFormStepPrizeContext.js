0dd9fd1395b78b8fa62d7595beecfaacdf84d74b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-16 00:56:19 +0700

    Create context class for AddCompetitionFormStepPrize

---
+++ components/competition-add/AddCompetitionFormStepPrizeContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * AddCompetitionFormStepPrizeContext
+ *
+ * @extends {BaseFuroContext<null>}
+ */
+export default class AddCompetitionFormStepPrizeContext extends BaseFuroContext {
+
+}
