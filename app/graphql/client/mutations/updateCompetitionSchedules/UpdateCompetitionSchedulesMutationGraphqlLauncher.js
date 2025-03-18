ef17afc855c4975955fb87013d518b842778ac19
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-18 18:14:24 +0700

    Add GraphQL definitions for `updateCompetitionSchedules` mutation

---
+++ app/graphql/client/mutations/updateCompetitionSchedules/UpdateCompetitionSchedulesMutationGraphqlLauncher.js
@@ -0,0 +1,20 @@
+import BaseAppGraphqlLauncher from '~/app/graphql/client/BaseAppGraphqlLauncher'
+import UpdateCompetitionSchedulesMutationGraphqlPayload from './UpdateCompetitionSchedulesMutationGraphqlPayload'
+import UpdateCompetitionSchedulesMutationGraphqlCapsule from './UpdateCompetitionSchedulesMutationGraphqlCapsule'
+
+/**
+ * UpdateCompetitionSchedulesMutation graphql launcher
+ *
+ * @extends {BaseAppGraphqlLauncher<typeof UpdateCompetitionSchedulesMutationGraphqlLauncher>}
+ */
+export default class UpdateCompetitionSchedulesMutationGraphqlLauncher extends BaseAppGraphqlLauncher {
+  /** @override */
+  static get Payload () {
+    return UpdateCompetitionSchedulesMutationGraphqlPayload
+  }
+
+  /** @override */
+  static get Capsule () {
+    return UpdateCompetitionSchedulesMutationGraphqlCapsule
+  }
+}
