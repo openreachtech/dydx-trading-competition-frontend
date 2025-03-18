5847e97ec77e4b882cd8739c3880750bfd2a2515
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-18 17:56:51 +0700

    Add GraphQL definitions for `updateCompetition` mutation

---
+++ app/graphql/client/mutations/updateCompetition/UpdateCompetitionMutationGraphqlPayload.js
@@ -0,0 +1,40 @@
+import BaseAppSignatureGraphqlPayload from '~/app/graphql/client/BaseAppSignatureGraphqlPayload'
+
+/**
+ * UpdateCompetitionMutation graphql payload
+ *
+ * @extends {BaseAppSignatureGraphqlPayload<UpdateCompetitionMutationRequestVariables>}
+ */
+export default class UpdateCompetitionMutationGraphqlPayload extends BaseAppSignatureGraphqlPayload {
+  /** @override */
+  static get document () {
+    return /* GraphQL */ `
+      mutation UpdateCompetition ($input: UpdateCompetitionInput!) {
+        updateCompetition (input: $input) {
+          competitionId
+        }
+      }
+    `
+  }
+}
+
+/**
+ * @typedef {{
+ *   input: {
+ *     competitionId: number
+ *     title: string
+ *     description: string
+ *     minimumDeposit: string
+ *     totalPrize: string
+ *     imageId: number
+ *     participantLowerLimit: number
+ *     participantUpperLimit: number
+ *     signature: {
+ *       signDoc: string
+ *       signature: string
+ *       publicKey: string
+ *       address: string
+ *     }
+ *   }
+ * }} UpdateCompetitionMutationRequestVariables
+ */
