673115b7c7fe2f55b7e469957547fea719dadd64
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-20 01:41:35 +0700

    Build context class for CompetitionEnrollmentDialog

---
+++ components/dialogs/CompetitionEnrollmentDialogContext.js
@@ -0,0 +1,10 @@
+import AppDialogContext from '~/app/vue/contexts/AppDialogContext'
+
+/**
+ * CompetitionTermsDialogContext
+ *
+ * @extends {AppDialogContext}
+ */
+export default class CompetitionTermsDialogContext extends AppDialogContext {
+
+}
