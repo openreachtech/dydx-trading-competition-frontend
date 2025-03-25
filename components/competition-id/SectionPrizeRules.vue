ecc1c1a2c7f573f1969171fe943f30cd87d1ed7f
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-26 00:36:47 +0700

    Build SectionPrizeRules component

---
+++ components/competition-id/SectionPrizeRules.vue
@@ -0,0 +1,125 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import {
+  Icon,
+} from '#components'
+
+import SectionPrizeRulesContext from './SectionPrizeRulesContext'
+
+export default defineComponent({
+  components: {
+    Icon,
+  },
+
+  props: {
+    prizeRules: {
+      /** @type {import('vue').PropType<import('./SectionPrizeRulesContext').PropsType['prizeRules']>} */
+      type: Array,
+      required: true,
+    },
+  },
+
+  setup (
+    props,
+    componentContext
+  ) {
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = SectionPrizeRulesContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <section class="unit-section">
+    <div class="inner">
+      <div v-for="it of 6"
+        class="unit-rank"
+      >
+        <Icon name="heroicons:trophy"
+          size="1.5rem"
+        />
+
+        <div class="content">
+          <span class="label">Rank 1st</span>
+          <span class="prize">$30,000</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.unit-section {
+  margin-inline: calc(-1 * var(--size-body-padding-inline-mobile));
+
+  border-block-width: var(--size-thinnest);
+  border-block-style: solid;
+  border-block-color: var(--color-border-competition-section);
+
+  padding-block: 2rem;
+
+  background-color: var(--color-background-competition-section-darker);
+
+  @media (30rem < width) {
+    margin-inline: calc(-1 * var(--size-body-padding-inline-desktop));
+  }
+}
+
+.unit-section > .inner {
+  margin-inline: auto;
+
+  padding-inline: var(--size-body-padding-inline-mobile);
+
+  max-width: var(--size-body-max-width);
+
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  row-gap: 1rem;
+  column-gap: 2rem;
+
+  @media (30rem < width) {
+    padding-inline: var(--size-body-padding-inline-desktop);
+  }
+}
+
+.unit-rank {
+  padding-block: 0.625rem;
+  padding-inline: 0.75rem;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.unit-rank > .content {
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+}
+
+.unit-rank > .content > .label {
+  font-size: var(--font-size-base);
+  font-weight: 500;
+
+  color: var(--color-text-secondary);
+}
+
+.unit-rank > .content > .prize {
+  font-size: var(--font-size-large);
+  font-weight: 700;
+
+  color: var(--color-text-primary);
+}
+</style>
