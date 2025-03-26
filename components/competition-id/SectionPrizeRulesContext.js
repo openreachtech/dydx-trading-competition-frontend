fba618d3b98390685496e5cd134558104c1d096f
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-03-26 23:12:59 +0700

    Add logic to normalize ranking position label

--- components/competition-id/SectionPrizeRulesContext.js
+++ components/competition-id/SectionPrizeRulesContext.js
@@ -2,6 +2,10 @@ import {
   BaseFuroContext,
 } from '@openreachtech/furo-nuxt'
 
+import {
+  RANK_POSITION_SUFFIX_HASH,
+} from '~/app/constants'
+
 /**
  * SectionPrizeRulesContext
  *
@@ -16,10 +20,88 @@ export default class SectionPrizeRulesContext extends BaseFuroContext {
   get prizeRules () {
     return this.props.prizeRules
   }
+
+  /**
+   * Normalize prize rules.
+   *
+   * @returns {Array<NormalizedPrizeRule>}
+   */
+  normalizePrizeRules () {
+    return this.prizeRules
+      .map(prizeRule => ({
+        label: this.generateRankLabel({
+          rankFrom: prizeRule.rankFrom,
+          rankTo: prizeRule.rankTo,
+        }),
+        amount: '$100',
+      }))
+  }
+
+  /**
+   * Generate rank label.
+   *
+   * @param {{
+   *   rankFrom: number
+   *   rankTo: number
+   * }} params - Parameters.
+   * @returns {string}
+   */
+  generateRankLabel ({
+    rankFrom,
+    rankTo,
+  }) {
+    const normalizedRankFrom = this.generateRankPosition({
+      rank: rankFrom,
+    })
+
+    if (rankFrom === rankTo) {
+      return `Rank ${normalizedRankFrom}`
+    }
+
+    const normalizedRankTo = this.generateRankPosition({
+      rank: rankTo,
+    })
+
+    return `Rank ${normalizedRankFrom} - ${normalizedRankTo}`
+  }
+
+  /**
+   * Generate rank position.
+   *
+   * @param {{
+   *   rank: number
+   * }} params - Parameters.
+   * @returns {string}
+   */
+  generateRankPosition ({
+    rank,
+  }) {
+    const lastTwoDigits = rank % 100
+
+    if (
+      lastTwoDigits >= 11
+      && lastTwoDigits <= 13
+    ) {
+      return `${rank}${RANK_POSITION_SUFFIX_HASH.OTHER}`
+    }
+
+    const lastDigit = /** @type {keyof typeof RANK_POSITION_SUFFIX_HASH} */ (rank % 10)
+    const suffix = RANK_POSITION_SUFFIX_HASH[lastDigit]
+      ?? RANK_POSITION_SUFFIX_HASH.OTHER
+
+    return `${rank}${suffix}`
+  }
 }
 
 /**
  * @typedef {{
  *   prizeRules: import('~/app/graphql/client/queries/competition/CompetitionQueryGraphqlCapsule').CompetitionEntity['prizeRules']
  * }} PropsType
+ */
+
+/**
+ * @typedef {{
+ *   label: string
+ *   amount: string
+ * }} NormalizedPrizeRule
  */
