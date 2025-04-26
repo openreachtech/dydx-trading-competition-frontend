62346a7e2932d82958515e12b825ce18042177ae
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-26 09:49:16 +0700

    Create boilerplate for competition details edit page

---
+++ pages/(competitions)/competitions/[competitionId]/edit/CompetitionDetailsEditPageContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * CompetitionDetailsEditPageContext
+ *
+ * @extends {BaseFuroContext<null, {}, null>}
+ */
+export default class CompetitionDetailsEditPageContext extends BaseFuroContext {
+
+}
