f0fbf5ba7303ddcdf8639a8dfdbba8f485edc53a
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-26 09:49:16 +0700

    Create boilerplate for competition details edit page

---
+++ pages/(competitions)/competitions/[competitionId]/edit/index.vue
@@ -0,0 +1,31 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import CompetitionDetailsEditPageContext from './CompetitionDetailsEditPageContext'
+
+export default defineComponent({
+  setup (
+    props,
+    componentContext
+  ) {
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = CompetitionDetailsEditPageContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="unit-page">
+    Edit
+  </div>
+</template>
