a1e6708d230a92e30c67622421b0d8707c85e61c
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-04-29 17:49:40 +0700

    Correct edit competition header UX

--- pages/(competitions)/competitions/[competitionId]/edit/index.vue
+++ pages/(competitions)/competitions/[competitionId]/edit/index.vue
@@ -43,15 +43,19 @@ export default defineComponent({
 
 <template>
   <div class="unit-page">
-    <NuxtLink :to="context.generateCompetitionDetailsUrl()"
-      class="return"
-    >
-      <Icon name="heroicons:arrow-left"
-        size="1.5rem"
-      />
-
-      <span>Edit League</span>
-    </NuxtLink>
+    <div class="header">
+      <NuxtLink :to="context.generateCompetitionDetailsUrl()"
+        aria-label="Go back to competition details"
+        class="link back"
+      >
+        <Icon name="heroicons:arrow-left"
+          size="1.5rem"
+        />
+      </NuxtLink>
+      <span class="heading">
+        Edit League
+      </span>
+    </div>
   </div>
 </template>
 
@@ -68,21 +72,27 @@ export default defineComponent({
   }
 }
 
-.unit-page > .return {
-  display: inline-flex;
+.unit-page > .header {
+  display: flex;
   align-items: center;
   gap: 0.75rem;
 
+  color: var(--color-text-primary);
+}
+
+.unit-page > .header > .link {
+  color: inherit;
+}
+
+.unit-page > .header > .link.back {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.unit-page > .header > .heading {
   font-size: var(--font-size-large);
   font-weight: 500;
   line-height: var(--size-line-height-large);
-
-  color: var(--color-text-primary);
-
-  transition: gap 150ms var(--transition-timing-base);
-}
-
-.unit-page > .return:hover {
-  gap: 1rem;
 }
 </style>
