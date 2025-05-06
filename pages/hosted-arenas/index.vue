ec146b1542f2fc6cacb2140442c730aa389a58b3
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-06 12:38:39 +0700

    Create boilerplate for hosted arenas page

---
+++ pages/hosted-arenas/index.vue
@@ -0,0 +1,31 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import HostedArenasPageContext from './HostedArenasPageContext'
+
+export default defineComponent({
+  setup (
+    props,
+    componentContext
+  ) {
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = HostedArenasPageContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="unit-page">
+    Hosted Arenas
+  </div>
+</template>
