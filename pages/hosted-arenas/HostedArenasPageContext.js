5f71b65a5e5d7125a57ad6a754cfb4245942f183
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-06 15:10:41 +0700

    Fetch hosted competitions on route param changes

--- pages/hosted-arenas/HostedArenasPageContext.js
+++ pages/hosted-arenas/HostedArenasPageContext.js
@@ -73,7 +73,66 @@ export default class HostedArenasPageContext extends BaseFuroContext {
   setupComponent () {
     this.hostedArenasFetcher.fetchCompetitionsOnMounted()
 
+    this.watch(
+      [
+        () => this.extractTitleFromRoute(),
+        () => this.extractStatusIdFromRoute(),
+        () => this.extractCurrentPageFromRoute(),
+      ],
+      async () => {
+        await this.hostedArenasFetcher.fetchCompetitionsOnEvent()
+      }
+    )
+
     return this
+  }
+
+  /**
+   * Extract title from route.
+   *
+   * @returns {string | null}
+   */
+  extractTitleFromRoute () {
+    const queryTitle = Array.isArray(this.route.query.title)
+      ? this.route.query.title[0]
+      : this.route.query.title
+
+    return queryTitle
+      ?? null
+  }
+
+  /**
+   * Extract status id from route.
+   *
+   * @returns {number | null}
+   */
+  extractStatusIdFromRoute () {
+    const queryStatusId = Array.isArray(this.route.query.statusId)
+      ? this.route.query.statusId[0]
+      : this.route.query.statusId
+
+    const statusId = Number(queryStatusId)
+    if (isNaN(statusId)) {
+      return null
+    }
+
+    return statusId
+  }
+
+  /**
+   * Extract current page from route.
+   *
+   * @returns {number}.
+   */
+  extractCurrentPageFromRoute () {
+    const currentPageQuery = Array.isArray(this.route.query.page)
+      ? this.route.query.page[0]
+      : this.route.query.page
+    const currentPage = Number(currentPageQuery)
+
+    return isNaN(currentPage)
+      ? 1
+      : currentPage
   }
 }
 
