422aeb5bf22c51ca7d1cb03c14b239cf499475c7
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-07 18:04:33 +0700

    Create route for hosted arena details page

---
+++ pages/hosted-arenas/[arenaId]/HostedArenaDetailsPageContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * HostedArenaDetailsPageContext
+ *
+ * @extends {BaseFuroContext<null, {}, null>}
+ */
+export default class HostedArenaDetailsPageContext extends BaseFuroContext {
+
+}
