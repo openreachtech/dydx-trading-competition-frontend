3de46a3b2a84a8d1f805aa4235de65b24aafcafb
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-07 18:04:33 +0700

    Create route for hosted arena details page

---
+++ pages/hosted-arenas/[arenaId]/index.vue
@@ -0,0 +1,46 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import HostedArenaDetailsPageContext from './HostedArenaDetailsPageContext'
+
+export default defineComponent({
+  setup (
+    props,
+    componentContext
+  ) {
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = HostedArenaDetailsPageContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="unit-page">
+    Hosted Arena Details
+  </div>
+</template>
+
+<style scoped>
+.unit-page {
+  margin-inline: auto;
+
+  max-width: var(--size-body-max-width);
+
+  padding-block-end: 12rem;
+  padding-inline: var(--size-body-padding-inline-mobile);
+
+  @media (30rem < width) {
+    padding-inline: var(--size-body-padding-inline-desktop);
+  }
+}
+</style>
