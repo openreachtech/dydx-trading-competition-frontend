c7aa192b6cc7f1d384a49f47fc512c961fdff20a
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-08 15:29:09 +0700

    Build header for hosted competitions list

--- pages/hosted-competitions/[competitionId]/index.vue
+++ pages/hosted-competitions/[competitionId]/index.vue
@@ -12,12 +12,22 @@ import {
   useGraphqlClient,
 } from '@openreachtech/furo-nuxt'
 
+import {
+  NuxtLink,
+  Icon,
+} from '#components'
+
 import CompetitionQueryGraphqlLauncher from '~/app/graphql/client/queries/competition/CompetitionQueryGraphqlLauncher'
 
 import HostedCompetitionDetailsFetcher from './HostedCompetitionDetailsFetcher'
 import HostedCompetitionDetailsPageContext from './HostedCompetitionDetailsPageContext'
 
 export default defineComponent({
+  components: {
+    NuxtLink,
+    Icon,
+  },
+
   setup (
     props,
     componentContext
@@ -58,7 +68,28 @@ export default defineComponent({
 
 <template>
   <div class="unit-page">
-    Hosted Arena Details
+    <div class="header">
+      <NuxtLink
+        to="/hosted-competitions"
+        class="link back"
+        aria-label="See hosted arenas list"
+      >
+        <Icon
+          name="heroicons:arrow-left"
+          size="1.5rem"
+        />
+      </NuxtLink>
+
+      <img
+        class="image"
+        :src="context.generateImageUrl()"
+        :alt="context.generateTitle()"
+      >
+
+      <h2 class="heading">
+        {{ context.generateTitle() }}
+      </h2>
+    </div>
   </div>
 </template>
 
@@ -74,5 +105,47 @@ export default defineComponent({
   @media (30rem < width) {
     padding-inline: var(--size-body-padding-inline-desktop);
   }
+}
+
+.unit-page > .header {
+  margin-block-end: 2.25rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  color: var(--color-text-primary);
+}
+
+.unit-page > .header > .link {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  color: inherit;
+}
+
+.unit-page > .header > .link.back {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.unit-page > .header > .image {
+  border-radius: 0.625rem;
+
+  inline-size: 2.5rem;
+  block-size: 2.5rem;
+
+  padding-block: 0.25rem;
+  padding-inline: 0.25rem;
+
+  background-color: var(--color-background-skeleton);
+}
+
+.unit-page > .header > .heading {
+  font-size: var(--font-size-large);
+  font-weight: 700;
+  line-height: var(--size-line-height-large);
 }
 </style>
