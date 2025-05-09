6fd18ccb3d3b746f835a7e91ba9b6658510f6e81
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-05-09 16:26:04 +0700

    Build context for HostedCompetitionParticipants

---
+++ components/hosted-competition/HostedCompetitionParticipantsContext.js
@@ -0,0 +1,138 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * HostedCompetitionParticipantsContext
+ *
+ * @extends {BaseFuroContext<null, PropsType, null>}
+ */
+export default class HostedCompetitionParticipantsContext extends BaseFuroContext {
+  /**
+   * get: participants
+   *
+   * @returns {PropsType['participants']}
+   */
+  get participants () {
+    return this.props.participants
+  }
+
+  /**
+   * get: participantsHeaderEntries
+   *
+   * @returns {Array<import('~/app/vue/contexts/AppTableContext').HeaderEntry>}
+   */
+  get participantsTableHeaderEntries () {
+    return [
+      {
+        key: 'checkbox',
+        label: '',
+      },
+      {
+        label: 'Name',
+        key: 'name',
+      },
+      {
+        label: 'Wallet Address',
+        key: 'address',
+      },
+      {
+        label: 'Equity',
+        key: 'equity',
+      },
+      {
+        label: 'Status',
+        key: 'status',
+        columnOptions: {
+          textAlign: 'end',
+        },
+      },
+    ]
+  }
+
+  /**
+   * Generate participants table entries.
+   *
+   * @returns {Array<TableBodyEntry>}
+   */
+  generateParticipantsTableEntries () {
+    return this.participants.map(
+      participant => ({
+        name: participant.address.name,
+        address: participant.address.address,
+        equity: participant.equity,
+        status: participant.status.statusId,
+      })
+    )
+  }
+
+  /**
+   * Generate displayed participant address.
+   *
+   * @param {{
+   *   address: string | null
+   * }} params - Parameters.
+   * @returns {string}
+   */
+  generateDisplayedParticipantAddress ({
+    address,
+  }) {
+    if (address === null) {
+      return '----'
+    }
+
+    return this.shortenAddress({
+      address,
+    })
+  }
+
+  /**
+   * Shorten wallet address.
+   *
+   * @param {{
+   *   address: string
+   * }} params - Parameters
+   * @returns {string} Shortened address.
+   */
+  shortenAddress ({
+    address,
+  }) {
+    if (address.length <= 12) {
+      return address
+    }
+
+    const firstSevenCharacters = address.slice(0, 7)
+    const lastFiveCharacters = address.slice(-5)
+
+    return `${firstSevenCharacters}...${lastFiveCharacters}`
+  }
+
+  /**
+   * Generate host address URL on Mintscan.
+   *
+   * @param {{
+   *   address: string
+   * }} params - Parameters.
+   * @returns {string}
+   */
+  generateParticipantMintscanUrl ({
+    address,
+  }) {
+    return `https://www.mintscan.io/dydx/address/${address}`
+  }
+}
+
+/**
+ * @typedef {{
+ *   participants: Array<import('~/app/graphql/client/queries/competitionParticipants/CompetitionParticipantsQueryGraphqlCapsule').Participant>
+ * }} PropsType
+ */
+
+/**
+ * @typedef {{
+ *   name: string
+ *   address: string
+ *   equity: number
+ *   status: number
+ * }} TableBodyEntry
+ */
