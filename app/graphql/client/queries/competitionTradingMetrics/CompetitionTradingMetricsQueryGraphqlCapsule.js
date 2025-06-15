45f8deb1812a06a09472b3946bab38df4a27801b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-06-15 19:45:45 +0700

    Create capsule for competitionTradingMetrics query

---
+++ app/graphql/client/queries/competitionTradingMetrics/CompetitionTradingMetricsQueryGraphqlCapsule.js
@@ -0,0 +1,85 @@
+import BaseAppGraphqlCapsule from '~/app/graphql/client/BaseAppGraphqlCapsule'
+
+/**
+ * `competitionTradingMetrics` query graphql capsule.
+ *
+ * @extends {BaseAppGraphqlCapsule<CompetitionTradingMetricsQueryResponseContent>}
+ */
+export default class CompetitionTradingMetricsQueryGraphqlCapsule extends BaseAppGraphqlCapsule {
+  /**
+   * Extract `competitionTradingMetrics` value hash.
+   *
+   * @returns {CompetitionTradingMetricsQueryResponseContent['competitionTradingMetrics'] | null}
+   */
+  extractCompetitionTradingMetricsValueHash () {
+    return this.extractContent()
+      ?.competitionTradingMetrics
+      ?? null
+  }
+
+  /**
+   * get: metrics
+   *
+   * @returns {Array<TradingMetric>}
+   */
+  get metrics () {
+    return this.extractCompetitionTradingMetricsValueHash()
+      ?.metrics
+      ?? []
+  }
+
+  /**
+   * get: pagination
+   *
+   * @returns {Pagination | null}
+   */
+  get pagination () {
+    return this.extractCompetitionTradingMetricsValueHash()
+      ?.pagination
+      ?? null
+  }
+
+  /**
+   * get: totalCount
+   *
+   * @returns {number | null}
+   */
+  get totalCount () {
+    return this.pagination
+      ?.totalCount
+      ?? null
+  }
+}
+
+/**
+ * @typedef {{
+ *   competitionTradingMetrics: {
+ *     metrics: Array<TradingMetric>
+ *     pagination: Pagination
+ *   }
+ * }} CompetitionTradingMetricsQueryResponseContent
+ */
+
+/**
+ * @typedef {{
+ *   address: {
+ *     address: string
+ *     name: string
+ *   }
+ *   makerFees: number
+ *   takerFees: number
+ *   totalFees: number
+ *   makeVolume: number
+ *   takerVolume: number
+ *   totalVolume: number
+ *   calculatedAt: string // ISO string
+ * }} TradingMetric
+ */
+
+/**
+ * @typedef {{
+ *   totalCount: number
+ *   limit: number
+ *   offset: number
+ * }} Pagination
+ */
