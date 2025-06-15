a251bc82401aba6150db16317ce7b97d9c3bbcce
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-06-15 11:47:17 +0700

    Implement logic to display profile orders

--- components/profile/ProfileOrders.vue
+++ components/profile/ProfileOrders.vue
@@ -41,13 +41,162 @@ export default defineComponent({
   <div class="unit-container">
     <AppTable
       :header-entries="context.orderTableHeaderEntries"
-      :entries="[]"
-    />
+      :entries="context.generateOrderTableEntries()"
+    >
+      <template
+        #body-type="{
+          value,
+        }"
+      >
+        <span class="unit-column type">
+          {{ value.toLowerCase() }}
+        </span>
+      </template>
+
+      <template
+        #body-side="{
+          value,
+        }"
+      >
+        <span
+          class="unit-column side"
+          :class="{
+            buy: context.isBuySide({
+              side: value,
+            }),
+            sell: context.isSellSide({
+              side: value,
+            }),
+          }"
+        >
+          {{ value.toLowerCase() }}
+        </span>
+      </template>
+
+      <template
+        #body-orderValue="{
+          value,
+        }"
+      >
+        {{
+          context.formatCurrency({
+            figure: value,
+          })
+        }}
+      </template>
+
+      <template
+        #body-price="{
+          value,
+        }"
+      >
+        <span class="unit-column price">
+          {{
+            context.formatCurrency({
+              figure: value,
+            })
+          }}
+        </span>
+      </template>
+
+      <template
+        #body-triggerPrice="{
+          value,
+        }"
+      >
+        <span
+          class="unit-column trigger-price"
+          :class="{
+            null: context.isNullish({
+              value,
+            }),
+          }"
+        >
+          {{
+            context.formatCurrency({
+              figure: value,
+            })
+          }}
+        </span>
+      </template>
+
+      <template
+        #body-marginMode="{
+          value,
+        }"
+      >
+        <span class="unit-column margin-mode">
+          {{ value }}
+        </span>
+      </template>
+
+      <template
+        #body-goodTilBlockTime="{
+          value,
+        }"
+      >
+        <span>
+          {{
+            context.formatRelativeTime({
+              timestamp: value,
+            })
+          }}
+        </span>
+      </template>
+    </AppTable>
   </div>
 </template>
 
 <style scoped>
 .unit-container {
   margin-block-start: 2rem;
+}
+
+.unit-column.type {
+  text-transform: capitalize;
+}
+
+.unit-column.side {
+  --color-text-side-buy: var(--palette-green);
+  --color-text-side-sell: var(--palette-red);
+  --color-background-side-buy: var(--palette-green-faded);
+  --color-background-side-sell: var(--palette-red-faded);
+
+  border-radius: 0.25rem;
+
+  padding-block: 0.125rem;
+  padding-inline: 0.25rem;
+
+  font-size: var(--font-size-small);
+
+  text-transform: capitalize;
+}
+
+.unit-column.side.buy {
+  color: var(--color-text-side-buy);
+  background-color: var(--color-background-side-buy);
+}
+
+.unit-column.side.sell {
+  color: var(--color-text-side-sell);
+  background-color: var(--color-background-side-sell);
+}
+
+.unit-column.trigger-price.null {
+  color: var(--color-text-placeholder);
+}
+
+.unit-column.margin-mode {
+  --color-background-margin-mode: var(--palette-layer-6);
+
+  border-radius: 0.25rem;
+
+  padding-block: 0.125rem;
+  padding-inline: 0.25rem;
+
+  font-size: var(--font-size-small);
+  text-transform: capitalize;
+
+  background-color: var(--color-background-margin-mode);
 }
 </style>
--- components/profile/ProfileOrdersContext.js
+++ components/profile/ProfileOrdersContext.js
@@ -2,6 +2,11 @@ import {
   BaseFuroContext,
 } from '@openreachtech/furo-nuxt'
 
+const ORDER_SIDE = {
+  BUY: 'BUY',
+  SELL: 'SELL',
+}
+
 /**
  * ProfileOrdersContext
  *
@@ -25,11 +30,11 @@ export default class ProfileOrdersContext extends BaseFuroContext {
   get orderTableHeaderEntries () {
     return [
       {
-        key: 'market',
+        key: 'ticker',
         label: 'Market',
       },
       {
-        key: 'status',
+        key: 'type',
         label: 'Status',
       },
       {
@@ -37,11 +42,11 @@ export default class ProfileOrdersContext extends BaseFuroContext {
         label: 'Side',
       },
       {
-        key: 'amount',
+        key: 'size',
         label: 'Amount',
       },
       {
-        key: 'filled',
+        key: 'totalFilled',
         label: 'Filled',
       },
       {
@@ -53,14 +58,226 @@ export default class ProfileOrdersContext extends BaseFuroContext {
         label: 'Price',
       },
       {
-        key: 'trigger',
+        key: 'triggerPrice',
         label: 'Trigger',
       },
       {
         key: 'marginMode',
         label: 'Margin Mode',
       },
+      {
+        key: 'goodTilBlockTime',
+        label: 'Good Til',
+        columnOptions: {
+          textAlign: 'end',
+        },
+      },
     ]
+  }
+
+  /**
+   * Generate order table entries.
+   *
+   * @returns {Array<Partial<import('~/app/vue/contexts/profile/ProfileDetailsPageContext').ProfileOrder>>}
+   */
+  generateOrderTableEntries () {
+    return this.profileOrders.map(it => ({
+      ticker: it.ticker,
+      type: it.type,
+      side: it.side,
+      size: it.size,
+      totalFilled: it.totalFilled,
+      orderValue: this.calculateOrderValue({
+        size: it.size,
+        price: it.price,
+      }),
+      price: it.price,
+      triggerPrice: it.triggerPrice,
+      marginMode: this.generateDisplayedMarginMode({
+        subaccountNumber: it.subaccountNumber,
+      }),
+      goodTilBlockTime: it.goodTilBlockTime,
+    }))
+  }
+
+  /**
+   * Check if is "BUY" side order.
+   *
+   * @param {{
+   *   side: string
+   * }} params - Parameters.
+   * @returns {boolean}
+   */
+  isBuySide ({
+    side,
+  }) {
+    return side === ORDER_SIDE.BUY
+  }
+
+  /**
+   * Check if is "SELL" side order.
+   *
+   * @param {{
+   *   side: string
+   * }} params - Parameters.
+   * @returns {boolean}
+   */
+  isSellSide ({
+    side,
+  }) {
+    return side === ORDER_SIDE.SELL
+  }
+
+  /**
+   * Calculate order value.
+   *
+   * @param {{
+   *   size: string | number
+   *   price: string | number
+   * }} params - Parameters.
+   * @returns {number}
+   */
+  calculateOrderValue ({
+    size,
+    price,
+  }) {
+    const normalizedSize = typeof size === 'string'
+      ? parseFloat(size)
+      : size
+    const normalizedPrice = typeof price === 'string'
+      ? parseFloat(price)
+      : price
+
+    if (
+      isNaN(normalizedSize)
+      || isNaN(normalizedPrice)
+    ) {
+      return 0
+    }
+
+    return normalizedSize * normalizedPrice
+  }
+
+  /**
+   * Generate displayed margin mode.
+   *
+   * @param {{
+   *   subaccountNumber: number
+   * }} params - Parameters.
+   * @returns {'cross' | 'isolated'}
+   */
+  generateDisplayedMarginMode ({
+    subaccountNumber,
+  }) {
+    if (subaccountNumber >= 128) {
+      return 'isolated'
+    }
+
+    return 'cross'
+  }
+
+  /**
+   * Format currency.
+   *
+   * @param {{
+   *   figure: string | number
+   * }} params - Parameters.
+   * @returns {string}
+   */
+  formatCurrency ({
+    figure,
+  }) {
+    if (this.isNullish({
+      value: figure,
+    })) {
+      return '--'
+    }
+
+    const normalizedFigure = typeof figure === 'string'
+      ? parseFloat(figure)
+      : figure
+
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+
+    return formatter.format(normalizedFigure)
+  }
+
+  /**
+   * Format relative time.
+   *
+   * @param {{
+   *   timestamp: Date | string | number
+   * }} params - Parameters.
+   * @returns {string}
+   */
+  formatRelativeTime ({
+    timestamp,
+  }) {
+    if (this.isNullish({
+      value: timestamp,
+    })) {
+      return '--'
+    }
+
+    const date = new Date(timestamp)
+    const remainingTime = date.getTime() - Date.now()
+    const remainingDayCount = Math.floor(remainingTime / (1000 * 60 * 60 * 24))
+
+    const formatter = new Intl.RelativeTimeFormat('en-US', {
+      numeric: 'always',
+      style: 'narrow',
+    })
+    const unit = this.generateRelativeTimeUnit({
+      remainingDayCount,
+    })
+
+    return formatter.format(
+      remainingDayCount,
+      unit
+    )
+      .replace('in ', '')
+  }
+
+  /**
+   * Generate relative time unit.
+   *
+   * @param {{
+   *   remainingDayCount: number
+   * }} params - Parameters.
+   * @returns {Intl.RelativeTimeFormatUnit}
+   */
+  generateRelativeTimeUnit ({
+    remainingDayCount,
+  }) {
+    if (remainingDayCount >= 30) {
+      return 'month'
+    }
+
+    if (remainingDayCount >= 7) {
+      return 'week'
+    }
+
+    return 'day'
+  }
+
+  /**
+   * Check if a value is null or undefined.
+   *
+   * @param {{
+   *   value: *
+   * }} params - Parameters.
+   * @returns {boolean}
+   */
+  isNullish ({
+    value,
+  }) {
+    return value === null
+      || value === undefined
   }
 }
 
