6fe830702035ed9dd2e9ba2e6886afe2266c31ff
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-06-16 10:39:34 +0700

    Build `ProfileTrades` component

---
+++ components/profile/ProfileTrades.vue
@@ -0,0 +1,37 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import ProfileTradesContext from './ProfileTradesContext'
+
+export default defineComponent({
+  setup (
+    props,
+    componentContext
+  ) {
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = ProfileTradesContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="unit-container">
+    Profile Trades
+  </div>
+</template>
+
+<style scoped>
+.unit-container {
+  margin-block-start: 2rem;
+}
+</style>
---
+++ components/profile/ProfileTradesContext.js
@@ -0,0 +1,12 @@
+import {
+  BaseFuroContext,
+} from '@openreachtech/furo-nuxt'
+
+/**
+ * ProfileTradesContext
+ *
+ * @extends {BaseFuroContext<null, {}, null>}
+ */
+export default class ProfileTradesContext extends BaseFuroContext {
+
+}
