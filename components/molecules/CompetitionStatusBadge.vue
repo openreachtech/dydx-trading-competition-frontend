493620ee02ff585be154936c04cc72d8f17186f8
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2025-11-11 18:56:11 +0700

    Build CompetitionStatusBadge component

---
+++ components/molecules/CompetitionStatusBadge.vue
@@ -0,0 +1,50 @@
+<script>
+import {
+  defineComponent,
+} from 'vue'
+
+import AppIconBadge from '~/components/badges/AppIconBadge.vue'
+
+import CompetitionStatusBadgeContext from './CompetitionStatusBadgeContext'
+
+export default defineComponent({
+  components: {
+    AppIconBadge,
+  },
+
+  props: {
+    statusId: {
+      type: [
+        Number,
+        null,
+      ],
+      required: true,
+    },
+  },
+
+  setup (
+    props,
+    componentContext
+  ) {
+    const args = {
+      props,
+      componentContext,
+    }
+    const context = CompetitionStatusBadgeContext.create(args)
+      .setupComponent()
+
+    return {
+      context,
+    }
+  },
+})
+</script>
+
+<template>
+  <AppIconBadge
+    :icon-name="context.generateBadgeIconName()"
+    :severity="context.generateBadgeSeverity()"
+  >
+    {{ context.generateBadgeCaption() }}
+  </AppIconBadge>
+</template>
